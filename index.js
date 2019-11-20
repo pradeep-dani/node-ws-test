94c87300ce9192110fc4b3a3e76f087b804a1a73
Pradeepkumar Dani <pradeep.jdani@gmail.com>
2019-11-20 19:00:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,11 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  //var id = setInterval(function() {
-   // ws.send(JSON.stringify(new Date()), function() {  })
-  //}, 1000)
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
 
   console.log("websocket connection open")
-  
-  wss.on("message", function incoming(data) {
-    console.log(data+'recieved in Heroku');
-    // if you want to send that message back to the client who sent it, 
-    // you can use send method on the socket
-     ws.send(data + 'Sent from Heroku', function() {  })
-  });
   
   
   ws.on("close", function() {
